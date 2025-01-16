ddda87a815ccbb6e6800a5594d7b86c7a43cc64a
tderamus <deramust@gmail.com>
2025-01-15 19:24:06 -0600

    setup all products in Products class. Removed DisplayMenu.cs file as it was not needed. Moved display menu class to MenuItem.cs file. Refractored Program.cs file display menu method calls to pull from MenuItem file. Tested functionality

--- MenuItem.cs
+++ MenuItem.cs
@@ -5,3 +5,57 @@ public class MenuItem
 
 }
 
+
+
+// Create class to display menu items
+
+public class DisplayMenu
+{
+    public void DisplayMenuItems(List<MenuItem> menuItems)
+    {
+        bool exit = false;
+
+        while (!exit)
+        {
+            Console.WriteLine("\nPlease select an option from the menu below:");
+            for (int i = 0; i < menuItems.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {menuItems[i].Option}");
+            }
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("View all products!");
+                    ProductInventory productInventory = new ProductInventory();
+                    productInventory.ViewAllProducts();
+                    break;
+                case "2":
+                    Console.WriteLine("Add a product to inventory!");
+                    ProductInventory addProductToInventory = new ProductInventory();
+                    addProductToInventory.AddProduct();
+                    break;
+                case "3":
+                    Console.WriteLine("Delete a product from inventory!");
+                    ProductInventory deleteProductFromInventory = new ProductInventory();
+                    deleteProductFromInventory.DeleteProduct();
+                    break;
+                case "4":
+                    Console.WriteLine("Update a product's details!");
+                    ProductInventory updateProductDetails = new ProductInventory();
+                    updateProductDetails.UpdateProduct();
+                    break;
+                case "5":
+                    Console.WriteLine("Exit product inventory application!");
+                    exit = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+
+        }
+
+    }
+}
